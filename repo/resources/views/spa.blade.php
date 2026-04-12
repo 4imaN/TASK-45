9f6995baea92e958c5e60dfa29d90db1595ee70f
Aiman Ashber <aiman.ashber@chromaway.com>
2026-04-12 12:45:24 +0300

    fix: conditionally load Vite assets only when manifest or hot file exists

--- repo/resources/views/spa.blade.php
+++ repo/resources/views/spa.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Campus Resource Platform</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
 </head>
 <body class="bg-gray-50 min-h-screen">
     <div id="app"></div>
